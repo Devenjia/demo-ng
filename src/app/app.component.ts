d6f946e9741aa01c0a45f3018170654f8b59f3a4
Devenjia <yuzhuo.jia@accenture.com>
2018-08-22 21:44:29 +0800

    [browe]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'my-app1';
-  public leftPicPath = '../assets/left.png';
-  public RightPicPath = '../assets/right.png';
-  public iCurrentSelIndex = 0;
-  public collection = [
-    {
-      name: 'Yangyi',
-      active: true,
-      path: '../assets/1'
-    },
-    {
-      name: 'Yonggeng',
-      active: false,
-      path: '../assets/2'
-    },
-    {
-      name: 'Peter',
-      active: false,
-      path: '../assets/3'
-    },
-    {
-      name: 'Liuao',
-      active: false,
-      path: '../assets/4'
-    }
-  ];
-  next() {
-    this._setSelectIndex(this.iCurrentSelIndex + 1);
-  }
-  prev() {
-    this._setSelectIndex(this.iCurrentSelIndex - 1);
-  }
-  showSelectPic(sIndex) {
-    for (var i = 0; i < this.collection.length; i++) {
-      this.collection[i].active = false;
-    }
-    this.collection[sIndex].active = true;
-    this.iCurrentSelIndex = sIndex;
-  }
-  _setSelectIndex(iNewIndex) {
-    if (this.iCurrentSelIndex === this.collection.length - 1 && iNewIndex > this.iCurrentSelIndex) {
-      this.iCurrentSelIndex = 0;
-    } else if (this.iCurrentSelIndex === 0 && iNewIndex < this.iCurrentSelIndex) {
-      this.iCurrentSelIndex = this.collection.length - 1;
-    } else {
-      this.iCurrentSelIndex = iNewIndex;
-    }
-    this.showSelectPic(this.iCurrentSelIndex);
-  }
+	title = 'carousel';
+	public items = [
+		{
+			imgUrl: '/src/assets/images/1.png',
+			current: true
+		}, 
+		{
+			imgUrl: '/src/assets/images/2.png',
+			current: false
+		}, 
+		{
+			imgUrl: '/src/assets/images/3.png',
+			current: false
+		}, 
+		{
+			imgUrl: '/src/assets/images/4.png',
+			current: false
+		}, 
+		{
+			imgUrl: '/src/assets/images/5.png',
+			current: false
+		}, 
+		{
+			imgUrl: '/src/assets/images/6.png',
+			current: false
+		}
+	];
+	public currentSlide = 0;
+
+	constructor() {
+		// this.autoCarousel();
+	}
+
+	changeImg(index) {
+		this.items.forEach((val) => {
+			val.current = false;
+		});
+		this.currentSlide = index < 0 ? this.items.length - 1  : (index > this.items.length - 1 ? 0 : index) ;
+		this.items[this.currentSlide].current = true;
+		
+	}
+	
 }
